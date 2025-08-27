c37948a2f03e295ce8271caf479628bf67111fa3
Omkar1972 <omkarbagmare49@gmail.com>
2025-08-27 22:27:50 +0530

    first commit

---
+++ T1.java
@@ -0,0 +1,24 @@
+import java.util.TreeSet;
+
+public class T1 {
+
+	public static void main(String[] args) {
+		TreeSet<String> ts=new TreeSet<String>();
+		
+		ts.add("Black");
+		ts.add("Green");
+		ts.add("Orange");
+		ts.add("Red");
+		ts.add("White");
+		
+		System.out.println("Tree Set: "+ts);
+
+	}
+
+}
+//Test input:
+//Black, Green, Orange, Red, White
+//
+//
+//Sample Output:Tree set:
+//[Black, Green, Orange, Red, White]
