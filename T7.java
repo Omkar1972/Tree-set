411c97b41a44d95656fc38ee9649e50ff2d1e8f3
Omkar1972 <omkarbagmare49@gmail.com>
2025-09-01 12:30:12 +0530

    Sixth commit

---
+++ T7.java
@@ -0,0 +1,21 @@
+import java.util.TreeSet;
+
+public class T7 {
+
+	public static void main(String[] args) {
+		
+     TreeSet<String> ts=new TreeSet<String>();
+		
+		ts.add("Black");
+		ts.add("Green");
+		ts.add("Orange");
+		ts.add("Red");
+		ts.add("White");
+		
+		System.out.println("Tree set: "+ts);
+		
+		System.out.println("Size of the tree set: "+ts.size());
+
+	}
+
+}
