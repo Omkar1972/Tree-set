81f1fe7af1e306bb6c445cb0eac1f781d92eb7e9
Omkar1972 <omkarbagmare49@gmail.com>
2025-09-02 22:08:26 +0530

    Sixth commit

---
+++ T11.java
@@ -0,0 +1,31 @@
+import java.util.TreeSet;
+
+public class T11 {
+
+	public static void main(String[] args) {
+		
+		TreeSet<Integer> ts=new TreeSet<Integer>();
+
+		ts.add(10);
+		ts.add(14);
+		ts.add(16);
+		ts.add(23);
+		ts.add(25);
+		ts.add(36);
+		ts.add(70);
+		ts.add(82);
+		ts.add(89);
+		
+		System.out.println("Original tree set: "+ts);
+		
+	
+	}
+
+}
+//Test input:
+//[10, 14, 16, 22, 25, 36, 70, 82, 89]
+//
+//
+//Sample Output:Original tree set: [10, 14, 16, 22, 25, 36, 70, 82, 89]
+//Removes the last element: 89
+//Tree set after removing last element: [10, 14, 16, 22, 25, 36, 70, 82]
